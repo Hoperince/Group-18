c84bc8971d2d40fbeb8b594a80ca23b520156157
Hoperince <70020032+Hoperince@users.noreply.github.com>
2024-05-15 22:17:26 +0700

    Delete Website directory

--- Website/detail.php
+++
@@ -1,4 +0,0 @@
-<?php include "headernguoidung.php";?>
-<?php include "detail1.php";?>
-<?php include "detail2.php";?>
-<?php include "footernguoidung.php";?>
--- Website/xoahoadon.php
+++
@@ -1,9 +0,0 @@
-<?php include "headerquantri.php";?>
-<?php include "function_hoadon.php";?>
-<?php
-$hoadon = new hoadon();
-$id = $_GET['id'];
-$result = $hoadon->hienthiid($id);
-$row = mysqli_fetch_array($result);
-$hoadon->delete($id);
-?>
